3569ca1e9debcb25dadf976c86be132802e54103
Thomas Fett <user@thomasfett.de>
2013-11-03 15:44:07 +0100

    Enhanced sounds for arm movements.

--- js/helper.js
+++ js/helper.js
@@ -12,11 +12,28 @@ var $ = function(selector) {
 	return document.querySelector(selector);
 };
 
+var soundIsPlaying = false;
+
 var makeMove = function(elem,cssClass) {
-    soul.createStaticSound({"volume":50,"panning":0,"pitch":318,"character":0,"fatness":0,"noise":0,"damping":0,"attack":5,"release":10,"modulation":false,"delayTime":0,"delayVolume":50,"delayRepeat":false,"reverbTime":0,"reverbSpeed":50,"duration":152,"bang":false,"timePitch1":true,"timePitch1Value":422,"timePitch1Start":1,"timePitch1Duration":168,"timePitch2":false,"sequence":false}).play();
-	elem.className += ' ' + cssClass;
+    if ( soundIsPlaying === false ) {
+        if ( cssClass === 'up' ) {
+            soul.createStaticSound({"volume":80,"panning":0,"pitch":318,"character":0,"fatness":2,"noise":10,"damping":64,"attack":0,"release":0,"modulation":false,"delayTime":0,"delayVolume":50,"delayRepeat":false,"reverbTime":0,"reverbSpeed":50,"duration":300,"bang":false,"timePitch1":true,"timePitch2":true,"sequence":false,"timePitch1Value":468,"timePitch1Start":1,"timePitch1Duration":150,"timePitch2Value":318,"timePitch2Start":151,"timePitch2Duration":150}).play();
+        }
+        else if ( cssClass === 'middle' ) {
+            soul.createStaticSound({"volume":80,"panning":0,"pitch":318,"character":0,"fatness":2,"noise":10,"damping":64,"attack":0,"release":0,"modulation":false,"delayTime":0,"delayVolume":50,"delayRepeat":false,"reverbTime":0,"reverbSpeed":50,"duration":300,"bang":false,"timePitch1":true,"timePitch2":true,"sequence":false,"timePitch1Value":420,"timePitch1Start":1,"timePitch1Duration":150,"timePitch2Value":318,"timePitch2Start":151,"timePitch2Duration":150}).play();
+        }
+        else if ( cssClass === 'down' ) {
+            soul.createStaticSound({"volume":80,"panning":0,"pitch":318,"character":0,"fatness":2,"noise":10,"damping":64,"attack":0,"release":0,"modulation":false,"delayTime":0,"delayVolume":50,"delayRepeat":false,"reverbTime":0,"reverbSpeed":50,"duration":300,"bang":false,"timePitch1":true,"timePitch2":true,"sequence":false,"timePitch1Value":366,"timePitch1Start":1,"timePitch1Duration":150,"timePitch2Value":318,"timePitch2Start":151,"timePitch2Duration":150}).play();
+        }
+	    soundIsPlaying = true;  
+    }
+
+    elem.className += ' ' + cssClass;
     setTimeout(function() {
         elem.className = elem.className.replace(' ' + cssClass,'');
-        soul.createStaticSound({"volume":50,"panning":0,"pitch":442,"character":0,"fatness":0,"noise":0,"damping":0,"attack":16,"release":10,"modulation":false,"delayTime":0,"delayVolume":50,"delayRepeat":false,"reverbTime":0,"reverbSpeed":50,"duration":168,"bang":true,"timePitch1":true,"timePitch1Value":256,"timePitch1Start":1,"timePitch1Duration":168,"timePitch2":false,"sequence":false}).play();
     },150);
+
+    setTimeout( function() {
+        soundIsPlaying = false;
+    }, 300);
 };
