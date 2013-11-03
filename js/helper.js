8895133ce33e8c52b6879b2b9e32b83e2acb3c80
Thomas Fett <user@thomasfett.de>
2013-11-03 15:25:54 +0100

    Added first sounds to the makeMove method.

--- js/helper.js
+++ js/helper.js
@@ -13,8 +13,10 @@ var $ = function(selector) {
 };
 
 var makeMove = function(elem,cssClass) {
+    soul.createStaticSound({"volume":50,"panning":0,"pitch":318,"character":0,"fatness":0,"noise":0,"damping":0,"attack":5,"release":10,"modulation":false,"delayTime":0,"delayVolume":50,"delayRepeat":false,"reverbTime":0,"reverbSpeed":50,"duration":152,"bang":false,"timePitch1":true,"timePitch1Value":422,"timePitch1Start":1,"timePitch1Duration":168,"timePitch2":false,"sequence":false}).play();
 	elem.className += ' ' + cssClass;
     setTimeout(function() {
         elem.className = elem.className.replace(' ' + cssClass,'');
+        soul.createStaticSound({"volume":50,"panning":0,"pitch":442,"character":0,"fatness":0,"noise":0,"damping":0,"attack":16,"release":10,"modulation":false,"delayTime":0,"delayVolume":50,"delayRepeat":false,"reverbTime":0,"reverbSpeed":50,"duration":168,"bang":true,"timePitch1":true,"timePitch1Value":256,"timePitch1Start":1,"timePitch1Duration":168,"timePitch2":false,"sequence":false}).play();
     },150);
 };
