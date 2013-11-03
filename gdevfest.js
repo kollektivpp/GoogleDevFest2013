b1a895bcf5e0b3526341976d3ed53e6988210500
christian-bromann <contact@christian-bromann.com>
2013-11-03 13:38:59 +0100

    added socket.io and an example website

--- gdevfest.js
+++ gdevfest.js
@@ -1,48 +1,27 @@
 // var 
-    // app       = require('http').createServer(),
-    // io        = require('socket.io').listen(app),
-var arduino   = require('duino'),
+var app       = require('http').createServer(),
+    io        = require('socket.io').listen(app),
+    arduino   = require('duino'),
     board     = new arduino.Board({ debug: true, baudrate: 9600 }),
     sockets   = [];
 
-// app.listen(8000);
-
-board.on('data', function(m){
-    console.log(m);
+io.sockets.on('connection', function (socket) {
+    sockets.push(socket);
 });
-
 
 var dataHandler = function(data) {
 
-    var msgBus = filterKey(data),
-        msg = clear(data);
-
     try {
-        msg = JSON.parse(msg);
+        msg = JSON.parse(data);
     } catch(e) {
         return;
     }
 
-    console.log(msg);
+    sockets.forEach(function(socket) {
+        socket.emit('button', msg);
+    });
 
 };
 
-var clear = function(data) {
-    data = data.substr(3).replace(/\r\n/,'');
-
-    return data.substr(0,data.length - 3);
-};
-
-var filterKey = function(data) {
-    var firstBytes = data.substr(0,3),
-        lastBytes = data.replace(/\r\n/,'').substr(data.length - 3),
-        key = firstBytes[1],
-        delimiter = firstBytes[0];
-
-    if(firstBytes[2] === delimiter && lastBytes[1] === delimiter &&
-       lastBytes[0] === key && lastBytes[2] === key) {
-        return parseInt(key,10);
-    } else {
-        return 0;
-    }
-};
+app.listen(8000);
+board.on('data', dataHandler);
