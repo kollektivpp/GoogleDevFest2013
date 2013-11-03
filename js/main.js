d5490a856664361d78d26a3ceb8b1a955c23d051
Thomas Fett <user@thomasfett.de>
2013-11-03 16:59:03 +0100

    thriller sound

--- js/main.js
+++ js/main.js
@@ -45,6 +45,7 @@ socket.on('button',function(msg) {
 socket.on('disco', function(msg) {
     if(msg.state) {
         $('#bg-wrapper').className = ' disco';
+        soul.createStaticSound({"volume":75,"panning":0,"pitch":608,"character":0,"fatness":0,"noise":0,"damping":0,"attack":0,"release":0,"modulation":true,"delayTime":0,"delayVolume":50,"delayRepeat":false,"reverbTime":0,"reverbSpeed":50,"duration":4152,"bang":false,"timePitch1":false,"timePitch2":false,"sequence":true,"sequenceObjects":[{"pitch":568,"duration":2},{"pitch":593,"duration":2},{"pitch":568,"duration":2},{"pitch":608,"duration":3},{"pitch":593,"duration":13},{"pitch":20,"duration":1},{"pitch":593,"duration":4},{"pitch":585,"duration":4},{"pitch":568,"duration":4}],"modPitch":true,"modVolume":false,"modDamping":false,"modIntensity":2,"modSpeed":97,"modCharacter":1}).play();
     } else {
         $('#bg-wrapper').className = '';
     }
